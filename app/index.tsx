29e8c81a001e8db47a84738d6b2b5749091954e6
Anna Hellen <annamoura@alunos.utfpr.edu.br>
2025-05-17 19:05:33 -0300

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Link, Stack } from 'expo-router';
+
+import { Button } from '~/components/Button';
+import { Container } from '~/components/Container';
+import { ScreenContent } from '~/components/ScreenContent';
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Home' }} />
+      <Container>
+        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
+        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
+          <Button title="Show Details" />
+        </Link>
+        <Link href="/exercicios" asChild className="mt-4">
+          <Button title="Ver Exercícios" />
+        </Link>
+      </Container>
+    </>
+  );
+}
